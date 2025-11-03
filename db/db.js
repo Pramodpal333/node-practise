9af0f3f40210d665c136ce4099e1777df138c26e
Pramodpal333 <your.email@example.com>
2025-11-03 18:54:03 +0530

    refactored code

---
+++ db/db.js
@@ -0,0 +1,6 @@
+
+//Temp DB
+exports.BOOKS = [
+    {'id':1, 'title': '48 Laws of power', 'auther': 'Rober greenc'},
+    {'id':2, 'title': 'Steal like an artist', 'auther': 'xyzzz'},
+];
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bookRouter = require('./routes/book.routes')
 
 const app = express();
 
@@ -6,71 +7,6 @@ const PORT = 3000;
 
 app.use(express.json());
 
-//Temp DB
-const books = [
-    {'id':1, 'title': '48 Laws of power', 'auther': 'Rober greenc'},
-    {'id':2, 'title': 'Steal like an artist', 'auther': 'xyzzz'},
-];
-
-app.get('/books',(req,res)=>{
-    res.json(books);
-
-});
-
-app.get('/books/:id',(req,res)=>{
-
-    const id = parseInt(req.params.id);
-
-    if(isNaN(id))
-        return res.status(400).json({'error': 'Inavlid request, is should be a number'});
-
-    const book = books.find(e=> e.id === id);
-
-    if(!book)
-        return res.status(404).json({'error':`Book with id ${id} not found`});
-
-    return res.json(book);
-
-});
-
-
-app.post('/books',(req,res)=>{
-
-    console.log(req.body);
-
-     const {title, auther} = req.body;
-
-    if(!title || title === '')
-        return res.status(400).json({error:'Title is required'});
-
-     if(!auther || auther === '')
-        return res.status(400).json({error:'Auther is required'});
-
-     const id = books.length +1;
-     const book = {id,title,auther};
-
-     books.push(book);
-   
-
-     res.status(201).json({success: true,message: 'Book added sucessfully'});
-
-});
-
-
-app.delete('/books/:id',(req,res)=>{
-
-      const id = parseInt(req.params.id);
-
-    if(isNaN(id))
-        return res.status(400).json({'error': 'Inavlid request, is should be a number'});
-
-    const indexToDelete = books.findIndex(e=> e.id === id);
-
-    if(indexToDelete < 0)
-        return res.status(404).json({messgae:'Book not found'});
-
-    books.splice(indexToDelete,1);
-    res.json({success: true,messgae: `Book deleted of ID ${id}`});
-})
+app.use('/books',bookRouter)
 
 app.listen(PORT,()=> console.log(`Server is up and running at port ${PORT}`));
