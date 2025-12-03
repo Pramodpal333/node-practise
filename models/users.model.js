915c9032909be4159b69380fa2d27e8077b59792
Pramod Pal <pramod.pal@neo-world.com>
2025-12-03 15:14:22 +0530

    Add user model with fields for first name, last name, email, password, salt, and timestamps

--- models/users.model.js
+++ models/users.model.js
@@ -1,5 +1,13 @@
-import { integer, pgTable, uuid } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, varchar, timestamp } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
+  firstName: varchar("first_name", { length: 255 }).notNull(),
+  lastName: varchar("last_name", { length: 255 }),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: text().notNull(),
+  salt: text().notNull(),
+
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
 });
